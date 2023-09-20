fcc6319a8876db75cfbb4430bf1be1437c4befec
Seokho <seokho0120@gmail.com>
2023-09-20 16:25:56 +0900

    🎨 페이지 레이아웃 스타일링

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Footer() {
-  return <p>{"Don't foger to CODE your DREAM || ALL Right Reserved"}</p>;
+  return (
+    <p className='bg-slate-900 text-white py-2 text-sm text-center'>
+      {"Don't foger to CODE your DREAM || ALL Right Reserved"}
+    </p>
+  );
 }
