6f5b1ca4f61feed68657b8c91070a36ce08e6d43
Seokho <seokho0120@gmail.com>
2023-09-21 18:11:48 +0900

    🎨 마크다운뷰어 - 스타일링, 신텍스 하이라이터, 이미지 태그 수정, remark-gfm버전 다운그레이드

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,7 @@
+import Image from 'next/image';
 import { getPostData } from '@/service/posts';
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import ReactDom from 'react-dom';
 import MarkdownViewer from '@/components/MarkdownViewer';
+import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
   params: {
@@ -11,12 +10,29 @@ type Props = {
 };
 
 export default async function PostPage({ params: { slug } }: Props) {
-  const posts = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
+  // const posts = await getPostData(slug);
+  console.log('path', path);
 
   return (
-    <>
-      <h1>{posts.title}</h1>
-      <MarkdownViewer content={posts.content} />
-    </>
+    <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
+      <Image
+        className='w-full h-1/5 max-h-[500px]'
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className='flex flex-col p-4'>
+        <div className='flex items-center self-end text-sky-600'>
+          <AiTwotoneCalendar />
+          <p className='font-semibold ml-2'>{date.toString()}</p>
+        </div>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p className='text-xl font-bold'>{description}</p>
+        <div className='w-44 border-2 border-sky-600 mt-4 mb-8' />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
