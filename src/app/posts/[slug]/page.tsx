00162351d38a410e0434cf06983e999eb5654a47
Seokho <seokho0120@gmail.com>
2023-09-21 17:09:35 +0900

    🎨 마크다운뷰어 - 라이브러리 연동, 테일윈드 설정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,8 @@
 import { getPostData } from '@/service/posts';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import ReactDom from 'react-dom';
+import MarkdownViewer from '@/components/MarkdownViewer';
 
 type Props = {
   params: {
@@ -8,10 +12,11 @@ type Props = {
 
 export default async function PostPage({ params: { slug } }: Props) {
   const posts = await getPostData(slug);
+
   return (
     <>
       <h1>{posts.title}</h1>
-      <pre>{posts.content}</pre>
+      <MarkdownViewer content={posts.content} />
     </>
   );
 }
