77deeddb06b3dba73200e85d6f0894ed9866d7fd
Seokho <seokho0120@gmail.com>
2023-09-21 21:09:25 +0900

    🎨 about, contact 페이지 구현

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import Hero from '@/components/Hero';
+
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
 
 export default function AboutPage() {
-  return <p>나의 대해 쓰기!!</p>;
+  return (
+    <>
+      <Hero />
+      <section className='bg-gray-100 shadow-lg p-8 m-8 text-center'>
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          이직을 준비하는 프론트엔드 개발자 <br />
+          사람과 디자인을 담는 웹앱을 만들고 있음
+        </p>
+        <h2 className={TITLE_CLASS}>Carrer</h2>
+        <p>
+          바알 1년 <br />
+          에프앤에프 3개월
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>React, React-Native, TypeScript, JavaScript</p>
+      </section>
+    </>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,36 @@
+import ContactForm from '@/components/ContactForm';
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
+
+const LINKS = [
+  { icon: <AiFillGithub />, url: 'https://github.com/Seokho0120' },
+  { icon: <AiFillLinkedin />, url: 'https://github.com/Seokho0120' },
+  { icon: <AiFillYoutube />, url: 'https://github.com/Seokho0120' },
+];
+
+// 웹 페이지내에서 경로 이동은  <Link/> 사용
+// 외부 경로는 <a></a> 사용
 
 export default function ContactPage() {
-  return <p>연락하기!!!</p>;
+  return (
+    <section className='flex flex-col items-center'>
+      <h2 className='text-3xl font-bold my-2'>Contact Me</h2>
+      <p>seokho0120@gamil.com</p>
+      <ul className='flex gap-5 my-2'>
+        {LINKS.map((link, index) => (
+          <a
+            href={link.url}
+            target='_blank'
+            rel='noreferrer'
+            key={index}
+            className='text-5xl hover:text-yellow-400'
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className='text-3xl font-bold my-8'>Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
